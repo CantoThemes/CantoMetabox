e6fa2d64483d3efaa8f439f7d7f369e11c62fda9
rajuahmmed <rajuahmmed.cse@gmail.com>
2016-05-21 09:13:19 +0000

    JS var method from core

--- cantometabox.addon.class.php
+++ cantometabox.addon.class.php
@@ -14,18 +14,15 @@ class CantoMetabox extends CTF_Addon{
         
         $this->add_js_tmlp_to_admin_footer();
 
-        add_action('admin_head', array($this,'print_window_js_var'));
         
     }
     
     public function print_window_js_var (){
+        parent::print_window_js_var();
         ?>
         <script type="text/javascript">
             window.ctfmb_opts = {};
             window.ctfmb_values = {};
-            window.isAddon = true;
-    		window.ctf_fa_icons = <?php echo CTF_Help::get_icons_json(); ?>;
-    		window.ctf_google_fonts = <?php echo CTF_Help::get_google_font_json(); ?>;
     	</script>
         <?php
     }
